9b25a24a36f86f5874e6ce43930494be735e3d57
unknown <mu.yaser@hotmail.com>
2023-08-24 09:14:49 +0300

    building more reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -14,17 +14,25 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-
-        <Heading as="h2">Check in out</Heading>
-        <Button>Check in</Button>
-
-        <Input type="number" placeholder="Number of guests" />
-
-        <Heading as="h3">Form</Heading>
-        <Button>Check in</Button>
-
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in out</Heading>
+              <Button>Check in</Button>
+              <Button variation="secondary" size="large">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
