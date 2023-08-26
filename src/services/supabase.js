fa94922cf7e66f6e602c488c905991bd95828a26
Mu Yaser <mu.yaser@hotmail.com>
2023-08-26 16:42:35 +0300

    uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://doyomdhqqrgxznpwbcwo.supabase.co";
+export const supabaseUrl = "https://doyomdhqqrgxznpwbcwo.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRveW9tZGhxcXJneHpucHdiY3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI5NzQ3OTEsImV4cCI6MjAwODU1MDc5MX0.sYASC7CnY09SX9_iLUh7nrgeOuyESvCziubgEbGy3rE";
 const supabase = createClient(supabaseUrl, supabaseKey);
