1a75791f2bc50e418d3ad18223d2f2a42e05e302
unknown <mu.yaser@hotmail.com>
2023-08-28 09:32:02 +0300

    confirming cabin deletion

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
@@ -6,19 +5,14 @@ import Modal from "../../ui/Modal";
 function AddCabin() {
   return (
     <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      {/* <Modal.Open opens='table'>
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name='table'>
-        <CreateCabinForm />
-      </Modal.Window> */}
+      <div>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </div>
     </Modal>
   );
 }
