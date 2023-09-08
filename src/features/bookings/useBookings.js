befdccd9ac564451c83204f64aa32e1db370f89c
Mu Yaser <mu.yaser@hotmail.com>
2023-09-08 17:14:38 +0300

    API-side pagination bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -3,10 +3,10 @@ import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
-  const [serachParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
   // FILTER
-  const filterValue = serachParams.get("status");
+  const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
@@ -14,18 +14,21 @@ export function useBookings() {
   // { field: "totalPrice", value: 5000, method: "gte" };
 
   // SORT
-  const sortByRow = serachParams.get("sortBy") || "startDate-desc";
+  const sortByRow = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRow.split("-");
   const sortBy = { field, direction };
 
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, error, bookings };
+  return { isLoading, error, bookings, count };
 }
